79632b6af0e53297f62857f97eb08696dc873cd5
hwon22 <s2018s06@e-mirim.hs.kr>
2020-05-21 13:05:19 +0900

    6번

--- myrandomrealnumber/Program.cs
+++ myrandomrealnumber/Program.cs
@@ -10,7 +10,25 @@ namespace myrandomrealnumber
     {
         static void Main(string[] args)
         {
-
+            Random random = new Random();
+            int answer = random.Next() % 1000;
+            while (true)
+            {
+                Console.Write("숫자를 입력해주세요 :");
+                int input = int.Parse(Console.ReadLine());
+                if (answer == input)
+                {
+                    Console.WriteLine("정답입니다!\n");
+                }
+                else if (answer > input)
+                {
+                    Console.WriteLine(input+"보다는 큰 숫자입니다.\n.");
+                }
+                else
+                {
+                    Console.WriteLine(input + "보다는 작은 숫자입니다.\n.");
+                }
+            }
         }
     }
 }
